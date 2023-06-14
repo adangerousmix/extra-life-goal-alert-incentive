f0d34c4ac02b939352acde077cddb967ab5f37a1
ADangerousMix <ned@adangerousmix.com>
2023-06-14 14:38:30 -0400

    Updates to include game incentives.

--- widget.js
+++ widget.js
@@ -5,6 +5,7 @@ const progress = document.getElementsByClassName("goal-progress")[0];
 const alertUser = document.getElementsByClassName("alert-user")[0];
 const alertDonation = document.getElementsByClassName("alert-donation")[0];
 const alertMessage = document.getElementsByClassName("alert-message")[0];
+const alertIncentive = document.getElementsByClassName("alert-incentive")[0];
 
 let donations = [],
     donationGoal = 0,
@@ -16,15 +17,19 @@ let donations = [],
     goalMet = false,
     size = "{size}",
     sideDisplay = "{sideDisplay}",
+    donationAmount = 5,
+    twitch = "{twitch}",
+    games = "{games}",
     alerts = [],
     lastRand = 0,
     incentives = [],
     maxNumIncentives = 5,
     maxNumSounds = 10,
+    gameIncentives = [],
     elPing = 15000;
 
 let playAlert = (incentiveId) => {
-    console.log({enableAlerts})
+    console.log("Alerts enabled:", {enableAlerts})
     if ({enableAlerts} == true) {
         let soundAlert = [];
         let incentive = [];
@@ -56,7 +61,7 @@ let playAlert = (incentiveId) => {
     
             let rnd = randomNumber(soundAlert.sounds.length);
             let sound = new Audio(soundAlert.sounds[rnd].sound);
-            console.log(soundAlert.sounds[rnd].sound);
+            console.log("Sound ", soundAlert.sounds[rnd].sound);
             sound.play();
         } else {
             if (audio.src != "undefined") {
@@ -68,6 +73,7 @@ let playAlert = (incentiveId) => {
             alert.classList.add("alert-hide");
             void alert.offsetWidth;
             alert.classList.remove("alert-show");
+            alertIncentive.innerHTML = "";
         }, 8000);
     }
 };
@@ -81,7 +87,7 @@ let updateProgress = (percent) => {
 };
 
 let randomNumber = (numSounds) => {
-    console.log(numSounds);
+    console.log("Num sounds: ", numSounds);
     let newRand = Math.floor(Math.random() * Math.floor(numSounds));
 
     if (newRand == lastRand && numSounds != 1) {
@@ -104,28 +110,46 @@ let arrayColumn = (arr, n) => {
 };
 
 async function getLatestDonations() {
-    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/donations?where=createdDateUTC>%3D%27" + donations[0].createdDateUTC + "%27");
+    console.log("Date: ", donations[0].createdDateUTC);
+    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/donations?version=1.3&where=createdDateUTC>%3D%27" + donations[0].createdDateUTC + "%27&limit=20");
     const text = await response.text();
     return JSON.parse(text);
 }
 
 async function getELDetails() {
-    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}");
+    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}?version=1.3&limit=20");
     const text = await response.text();
     return JSON.parse(text);
 }
 
 async function getDonations() {
-    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/donations?limit=10");
+    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/donations?version=1.3&limit=20");
     const text = await response.text();
     donations = JSON.parse(text);
 }
 
 async function getIncentives() {
-    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/incentives");
+    const response = await fetch("https://extralife.donordrive.com/api/{ExtraLifeType}/{participantId}/incentives?version=1.3&limit=20");
     const text = await response.text();
     incentives = JSON.parse(text);
     console.log("Incentives: ", incentives);
+}
+
+async function gameIncentive() {
+    console.log("Game Incentive checked.");
+    let response = await fetch('https://decapi.me/twitch/game/' + twitch);
+    let game = await response.text();
+  
+    let gamesInc = games.split(";");
+  
+    if (gamesInc.includes(game)) {
+        let rng = Math.floor(Math.random() * gameIncentives.length);
+      
+        console.log("Game: ", game);  
+        alertIncentive.innerHTML = gameIncentives[rng];
+    } else {
+      	console.log("No game match for incentives.");
+    }
 }
 
 function checkForDonation() {
@@ -135,6 +159,15 @@ function checkForDonation() {
                 if (!arrayColumn(donations, "donationID").includes(donos[i].donationID) && donos[i].createdDateUTC >= donations[0].createdDateUTC) {
                     donations.unshift(donos[i]);
                     donationSum = donationSum + donations[0].amount;
+                  
+                    console.log("Dono amount: ", donations[0].amount);
+                    console.log("Dono amount to fire: ", donationAmount);
+                    if (donations[0].amount >= donationAmount) {
+                        console.log("Game incentive should fire.");
+                        gameIncentive();
+                    } else {
+                        console.log("Game incentive not in dono check.");
+                    }
 
                     if (donationSum >= donationGoal && !goalMet) {
                         setTimeout(goalComplete.play(), 10000);
@@ -183,9 +216,15 @@ let setAlerts = (fields) => {
 }
 
 window.addEventListener("onWidgetLoad", async function (obj) {
+  	console.log("obj: ", obj);
+    donationAmount = obj.detail.fieldData["donation-amount"];
+    gameIncentives = obj.detail.fieldData["game-incentives"].split(";");
+  console.log("Game Incentives: ", gameIncentives);
     container.classList.toggle("size-" + size);
     goal.classList.toggle("goal-" + sideDisplay);
     alert.classList.toggle("alert-" + sideDisplay);
+  
+    
 
     if ("{participantId}" !== "") {
         getELDetails().then(data => {
@@ -212,6 +251,7 @@ window.addEventListener("onEventReceived", function (obj) {
     const event = obj.detail.event;
 
     if (event.listener === "widget-button" && event.field === "testAlert") {
+        gameIncentive();
         playAlert(0);
     }
 });
